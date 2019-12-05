263a228d7e66d2da8ad6b68f9e8ae120ac2b7805
William Bautista <william.b.g.1997@gmail.com>
2019-12-05 16:59:40 -0500

    Scroll animate

---
+++ src/js/modules/get-started.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+const buttons = document.querySelectorAll('.get-started');
+const form = document.getElementById('shorten-form');
+const offset = form.offsetTop - 150;
+
+const getStarted = () => {
+  buttons.forEach((el) => {
+    el.onclick = () => {
+      window.scrollTo({
+        top: offset,
+        left: 0,
+        behavior: 'smooth',
+      });
+
+      window.onscroll = () => {
+        if (window.scrollY === offset) form[0].focus();
+      };
+    };
+  });
+};
+
+export default getStarted;
